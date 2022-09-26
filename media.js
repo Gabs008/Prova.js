6d8c02cc1a80a62cc45139b50b666efbf766fb9b
Gabs008 <101597088+Gabs008@users.noreply.github.com>
2022-09-26 14:56:58 -0300

    Prova Java Script

---
+++ media.js
@@ -0,0 +1,15 @@
+var a = 7
+var b = 10
+var c = 8
+var d = 7
+
+var e = (a+b+c+d)/4
+
+if (e >=7){
+    console.log("A média final",e,"aluno aprovado!!!!")
+}else{
+    console.log("A média final",e,",aluno reprovado!!!!!")
+}
+
+
+
